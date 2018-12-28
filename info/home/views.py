7d39d7e562985bec647c1d3f9228d7a9189e20bc
sunlu <15150676601@163.com>
2018-12-28 21:12:30 +0800

    根路由基本实现

--- info/home/views.py
+++ info/home/views.py
@@ -1,13 +1,7 @@
-from flask import current_app
-
 from info.home import home_blu
-import logging  # python内置的日志模块，日志可以输出到控制台，也可以写入到文件汇总
-# flask 中内置（logger）的日志信息也是使用logging模块实现的，但是日志没有保存到文件汇中
+from flask import render_template
 
-# 使用蓝图来注册路由
+
 @home_blu.route("/")
 def index():
-    # 建立使用flask封装的logging 语法（会显示具体的错误位置）
-    # logging.error("出现了错误")
-    current_app.logger.error("出现了错误")
-    return "首页"
+    return render_template("index.html")
