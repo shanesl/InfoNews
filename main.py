96ed1912b16f882195bb2a3622b2f008fe18b625
sunlu <15150676601@163.com>
2018-12-28 19:21:57 +0800

    flask-session组件保存session 数据到redis数据库中

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+import base64
+import os
+from datetime import timedelta
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from redis import Redis
+from flask_session import Session
 
 app = Flask(__name__)
+
 
 # 定义配置类来封装配置信息
 class Config:
@@ -11,7 +17,11 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False  # 是否追踪数据库变化
     REDIS_HOST = "127.0.0.1"  # redis的连接地址  将自定义的配置也封装到Config类中
     REDIS_PORT = 6379
-
+    SESSION_TYPE = "redis"  # 设置session的存储方式 redis 性能好，方便设置过期时间
+    SESSION_REDIScover = Redis(host=REDIS_HOST, port=REDIS_PORT)  # 设置Redis连接对象，组件会使用该对象将session数据保存到redis中
+    SESSION_USE_SIGNER = True  # 设置sessionid 进行加密
+    SECRET_KEY = base64.b64encode(os.urandom(48)).decode()  # 设置session 的随机秘钥
+    PERMANENT_SESSION_LIFETIME = timedelta(days=7)  # 设置session 过期时间 组件默认支持过期时间
 
 
 # 从对象中加载配置
@@ -21,6 +31,8 @@ db = SQLAlchemy(app)
 # 创建redis连接对象
 rs = Redis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
 
+# 初始化session存储对象
+Session(app)
 
 @app.route("/")
 def index():
