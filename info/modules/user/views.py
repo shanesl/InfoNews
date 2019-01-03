1614739537825ba8a3e65f043cc4fe39f4376b5a
sunlu <15150676601@163.com>
2019-01-03 20:18:22 +0800

    子页面嵌套

--- info/modules/user/views.py
+++ info/modules/user/views.py
@@ -5,3 +5,9 @@ from info.modules.user import user_blu
 @user_blu.route("/user_info")
 def user_info():
     return render_template("news/user.html", user=g.user.to_dict())
+
+
+@user_blu.route("/base_info")
+def base_info():
+
+    return render_template("news/user_base_info.html")
